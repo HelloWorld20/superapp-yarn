f677135bc6577d65ef4a28d5bc6dfcce86e01233
Leon.W <weijianghong@tuzhanai.com>
2021-03-09 19:07:47 +0800

    feat: ✨  添加两个房源

---
+++ packages/template-apart-radar/src/pages/apart-radar/data/黄埔花园.ts
@@ -0,0 +1,51 @@
+const data: ApartmentData = {
+  name: "黄埔花园",
+  prise: 38734,
+  totalPrise: 300,
+  zone: "黄埔区",
+  district: "大沙东（老黄埔）",
+  advantage: "恒大物业、户型不错，地铁近。直达珠江新城地铁",
+  disadvatage: "小区环境很差",
+  lng: 113.45768,
+  lat: 23.098974,
+  selected: true,
+  score: {
+    transport: {
+      data: 80,
+      desc: "",
+    },
+    enviranment: {
+      data: 50,
+      desc: "",
+    },
+    education: {
+      data: 70,
+      desc: "",
+    },
+    quality: {
+      data: 70,
+      desc: "",
+    },
+    age: {
+      data: 70,
+      desc: "",
+    },
+    style: {
+      data: 80,
+      desc: "",
+    },
+    appreciation: {
+      data: 65,
+      desc: "",
+    },
+    equipment: {
+      data: 70,
+      desc: "",
+    },
+    prise: {
+      data: 75
+    }
+  },
+};
+
+export default data;
