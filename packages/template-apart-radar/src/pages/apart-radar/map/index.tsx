2d5612a0c9ababf95d81c4e48c7216226aa25cca
Leon.W <weijianghong@tuzhanai.com>
2021-03-10 18:18:20 +0800

    feat: ✨  腾讯地图换高德地图

--- packages/template-apart-radar/src/pages/apart-radar/map/index.tsx
+++ packages/template-apart-radar/src/pages/apart-radar/map/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
-import loadScript from 'ww-utils/lib/load-script'
+import loadScript from "ww-utils/lib/load-script";
 
 import "./index.css";
 interface IProps {
@@ -13,66 +13,37 @@ export default (props: IProps) => {
   const [ready, setReady] = useState<boolean>(false);
   useEffect(() => {
     loadScript(
-      "https://map.qq.com/api/gljs?v=1.exp&key=PSDBZ-UNLR2-I36UR-CSMLR-UN7B3-YJBTH"
+      "https://webapi.amap.com/maps?v=1.4.15&key=840f3a44903c1ffa87b9e93174422d7f"
     ).then((res) => {
       if (mapIns.current) return;
-      var center = new TMap.LatLng(23.072969, 113.324059); //设置中心点坐标
-      //初始化地图
 
-      mapIns.current = new TMap.Map(mapEl.current, {
-        center: center,
+      mapIns.current = new AMap.Map(mapEl.current, {
         zoom: 11.5,
-        // baseMap: {  // 设置卫星地图
-        //   type: 'satellite'
-        // }
+        center: [113.324059, 23.072969],
+        viewMode: "3D",
+      });
+      // 添加控件
+      AMap.plugin(["AMap.MapType"], () => {
+        // 地图类型切换控件
+        mapIns.current.addControl(new AMap.MapType());
       });
 
-      setTimeout(() => {
-        console.log("start", mapIns.current);
-        mapIns.current.setBaseMap([{ type: "satellite" }, { type: "vector" }]);
-        // mapIns.current.panTo(new TMap.LatLng(23.072969, 113.324059))
-      }, 3000);
-
-      mapMarker.current = new TMap.MultiMarker({
-        id: "marker-layer", //图层id
-        map: mapIns.current,
-        styles: {
-          //点标注的相关样式
-          marker: new TMap.MarkerStyle({
-            width: 25,
-            height: 35,
-            anchor: { x: 16, y: 32 },
-            src:
-              "https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/markerDefault.png",
-          }),
-        },
-      });
-      mapMarker.current.on("click", () => {});
-      // makeMark();
       setReady(true);
     });
   }, []);
 
   useEffect(() => {
     if (ready) {
-      makeMark();
+      const markers = props.cords.map((cord) => {
+        const res = new AMap.Marker({
+          icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+          position: cord.reverse(),
+        });
+        return res;
+      });
+      mapIns.current.add(markers);
     }
   }, [ready, props.cords]);
-
-  const makeMark = useCallback(() => {
-    if (!mapIns.current || props.cords.length === 0) return;
-    const geometries = props.cords.map((cord) => ({
-      // id: "demo",
-      styleId: "marker",
-      position: new TMap.LatLng(cord[0], cord[1]),
-      properties: {
-        title: "marker",
-      },
-    }));
-
-    mapMarker.current.setGeometries(geometries);
-
-  }, [props.cords]);
 
   return (
     <div
--- packages/template-apart-radar/src/pages/double-map/map.tsx
+++ packages/template-apart-radar/src/pages/double-map/map.tsx
@@ -8,14 +8,14 @@ interface IProps {
 export default () => {
   const mapEl = useRef<any>(null);
   const mapIns = useRef<any>(null);
-  useEffect(() => {
-    loadScript(
-      "https://map.qq.com/api/gljs?v=1.exp&key=PSDBZ-UNLR2-I36UR-CSMLR-UN7B3-YJBTH"
-    ).then(() => {
-      if(mapIns.current) return;
-      const center = new TMap.LatLng
-    });
-  }, []);
+  // useEffect(() => {
+  //   loadScript(
+  //     "https://map.qq.com/api/gljs?v=1.exp&key=PSDBZ-UNLR2-I36UR-CSMLR-UN7B3-YJBTH"
+  //   ).then(() => {
+  //     if(mapIns.current) return;
+  //     const center = new TMap.LatLng
+  //   });
+  // }, []);
   return (
     <div
       className="c-map"
