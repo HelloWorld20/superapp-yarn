ddfe530db7211f808a548681b47650c9f7fa530b
weijianghong <weijianghong@tuzhanai.com>
2021-03-14 22:15:14 +0800

    feat(config改造、利用在uplotsad-cdn上。删除ww-middleware。):

--- packages/template-apart-radar/upload.js
+++ packages/template-apart-radar/upload.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const Uploader = require('ww-tools-upload-cdn');
+const Config = require('ww-config').default;
 
-const uploader = new Uploader(path.resolve(__dirname, './dist'), path.resolve(__dirname, './config.json'));
+const config = new Config(path.resolve(__dirname, './config.json'));
+
+const uploader = new Uploader(path.resolve(__dirname, './dist'), config.get('COS'));
 
 uploader.uploadFolder();
